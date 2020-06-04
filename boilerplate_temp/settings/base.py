0e154baeab8e4cf34613c69b65ed543c87d2f573
ro <robin.8895rawat@gmail.com>
2020-06-04 23:33:44 +0530

    whitenoise

--- boilerplate_temp/settings/base.py
+++ boilerplate_temp/settings/base.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+
 
 # Application definition
 
