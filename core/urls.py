6be37092ac7232fe9ac67c717d432eede0146b63
ro <robin.8895rawat@gmail.com>
2020-06-04 21:36:52 +0530

    /

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from .views import HomeView, ItemDetailView, add_to_cart, remove_from_cart, Orde
 app_name = 'core'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path('/', HomeView.as_view(), name='home'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/',add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/',remove_from_cart, name='remove-from-cart'),
